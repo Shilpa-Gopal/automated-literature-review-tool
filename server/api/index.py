9c00522998be3945c5cc614669dacb59eb219cf4
Shilpa G <36858878+Shilpa-Gopal@users.noreply.github.com>
2025-04-01 20:14:39 -0400

    Update index.py

--- server/api/index.py
+++ server/api/index.py
@@ -5,8 +5,11 @@ import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Import your Flask app
-from server.app import app
+from server.app import create_app
 
-# Vercel serverless handler
+# Initialize the app
+app = create_app()
+
+# Vercel handler
 def handler(request, context):
     return app(request, context)
